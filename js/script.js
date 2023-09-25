9c1356fcff3c4bf050612c6ed8fa819ebdffb928
Dawid Osiński <dawidosinski59@gmail.com>
2023-09-25 16:43:35 +0200

    mobile menu open and close functionality added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const hamMenu = document.querySelector(".ham__menu");
-const btn = document.querySelector("#btnHamburger");
+const btnOpen = document.querySelector("#btnHamburger");
+const btnClose = document.querySelector("#buttonHamburger");
+const body = document.querySelector("body");
 
-btn.addEventListener("click", () => {
-    hamMenu.classList.remove("ham__menu--open")
+btnOpen.addEventListener("click", () => {
+    hamMenu.classList.remove("ham__menu--open");
+    body.classList.add("noScroll");
+});
+
+btnClose.addEventListener("click", () => {
+    hamMenu.classList.add("ham__menu--open");
+    body.classList.remove("noScroll");
 });
