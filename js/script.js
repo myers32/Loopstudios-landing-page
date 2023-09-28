df3933e99bfb75cf72b8a1cd04bf179fb7875205
Dawid Osiński <dawidosinski59@gmail.com>
2023-09-28 17:42:29 +0200

    animation added for menu on mobiles

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ const hamMenu = document.querySelector(".ham__menu");
 const btnOpen = document.querySelector("#btnHamburger");
 const btnClose = document.querySelector("#buttonHamburger");
 const body = document.querySelector("body");
+const overlay = document.querySelector(".ham__menu--overlay");
 
 btnOpen.addEventListener("click", () => {
-    hamMenu.classList.remove("ham__menu--open");
+    hamMenu.classList.remove("ham__menu--overlay");
     body.classList.add("noScroll");
+
 });
 
 btnClose.addEventListener("click", () => {
-    hamMenu.classList.add("ham__menu--open");
+    hamMenu.classList.add("ham__menu--overlay");
     body.classList.remove("noScroll");
 });
