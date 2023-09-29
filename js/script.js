69042c72f74f42159d254c3b922e04e9eeb5c836
Dawid Osiński <dawidosinski59@gmail.com>
2023-09-29 12:08:39 +0200

    overflow-y problem solved

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ const btnOpen = document.querySelector("#btnHamburger");
 const btnClose = document.querySelector("#buttonHamburger");
 const body = document.querySelector("body");
 const overlay = document.querySelector(".ham__menu--overlay");
+const html = document.querySelector("html");
 
 btnOpen.addEventListener("click", () => {
     hamMenu.classList.remove("ham__menu--overlay");
     body.classList.add("noScroll");
+    html.classList.add("noScroll");
 });
 
 btnClose.addEventListener("click", () => {
     hamMenu.classList.add("ham__menu--overlay");
     body.classList.remove("noScroll");
+    html.classList.remove("noScroll");
 });
